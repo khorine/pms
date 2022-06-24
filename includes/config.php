3b02f75569c1c674bc4aa794920358fc2e36fd1c
khorine <khorine@yahoo.com>
2022-06-24 15:44:43 +0300

    first commit

---
+++ includes/config.php
@@ -0,0 +1,21 @@
+<?php
+
+error_reporting(1);
+defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+defined('DB_USER')   ? null : define("DB_USER", "root");
+defined('DB_PASS')   ? null : define("DB_PASS", "");
+defined('DB_NAME')   ? null : define("DB_NAME", "techsava_rivercourt");
+date_default_timezone_set("Africa/Nairobi");
+$root="http://localhost/property-rivercourt-test/";
+
+
+// error_reporting(1);
+// defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+// defined('DB_USER')   ? null : define("DB_USER", "techsava_property");
+// defined('DB_PASS')   ? null : define("DB_PASS", "Property#22");
+// defined('DB_NAME')   ? null : define("DB_NAME", "techsava_property_htest");
+// date_default_timezone_set("Africa/Nairobi");
+// $root="https://techsavanna.technology/property-rivercourt-test/";
+
+?> 
+
---
+++ modules/addproperty.php
@@ -0,0 +1,40 @@
+<?php
+include '../views/display.php';
+include 'inputforms.php';
+$property=  getSettings();
+$baseurl=$baseurl;
+echo  $htmlheaders;
+echo '<head><title>Property Manager|'.$property['company_name'].'</title>';
+echo $meta;
+echo $overallcss;
+echo $jquery;
+//later set variable to session
+$admin='<u>'.$_SESSION['username'].'</u>';
+
+
+?>
+
+<script>
+$(function() {
+	$( "#menu" ).menu();
+});
+
+
+</script>
+<script src="../js/logout.js"></script>
+<style>
+.ui-menu { width: 150px;}
+</style>
+<?php 
+
+echo '</head><body>';
+echo '<div id="form">';
+echo '<h2>'.$wheat.$spacer.$logopath.$property['company_name'].' | Property Manager<div id="loggedin">'.$loggedin.' '.$admin.' | '.$clock.' '.$time.'</div>'.$endwheat.'</h2>' .'<hr/>'; 
+
+echo '<table id ="menulayout"><tr><td valign="top">';
+echo $sidebar;
+echo '</td><td>';
+echo addproperty();//inputforms
+echo '</td></tr></table></div>';
+echo '</body>';
+?>
